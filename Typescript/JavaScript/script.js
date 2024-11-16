dcee2ef5c936559a825c62d0e4b8d7eeffd1a8f2
Abdullah Hassan <65224472+abdullah12-alt@users.noreply.github.com>
2024-11-16 11:26:44 +0500

    js into

---
+++ Typescript/JavaScript/script.js
@@ -0,0 +1,14 @@
+// document.getElementsByTagName("p")[0].innerHTML= "hello this is js"
+
+
+// document.getElementById("heading1").innerHTML="this is heading from js"
+// document.getElementsByClassName("js-dom")[0].innerHTML="this is heading from class"
+
+function parkFun(){
+    window.alert("You are eligible to enter in the park")
+}
+
+
+// js tutorial link 
+
+// https://www.w3schools.com/js/js_output.asp
