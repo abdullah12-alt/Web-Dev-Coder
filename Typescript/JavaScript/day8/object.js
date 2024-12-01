8a24af6754ec6302558d831267dd48373bb61f45
Abdullah Hassan <65224472+abdullah12-alt@users.noreply.github.com>
2024-12-01 12:15:43 +0500

    objects

---
+++ Typescript/JavaScript/day8/object.js
@@ -0,0 +1,50 @@
+// there are two ways to declare object
+// literal
+// constructor
+
+// literal
+
+// const profile = {
+//     name : "Ahmed",
+//     age : 13,
+//     school: "amaroo secondary school, aus",
+//     nationality : "AUS",
+//     salary : "10$",
+//     profession : "young software engineer"
+// }
+
+// console.log(profile["age"])
+// console.log(profile.age)
+// console.log(profile.school)
+
+// change
+// profile.age=14
+// console.log(profile)
+
+
+// sal= 30
+
+// function salary(){
+//     return sal * 30 
+// }
+// const profile = {
+//     name : "Ahmed",
+//     age : 13,
+//     school: "amaroo secondary school, aus",
+//     nationality : "AUS",
+//     salary : salary(),
+//     profession : "young software engineer"
+// }
+
+// console.log(profile)
+
+// const profile = {
+//     name : "Ahmed",
+//     age : 13,
+//     school: "amaroo secondary school, aus",
+//     nationality : "AUS",
+//     salary : 300 + "$",
+//     profession : "young software engineer"
+// }
+
+// console.log(profile)
