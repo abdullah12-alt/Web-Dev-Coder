1f0ed6c93a498e9372e6321729e49d1fef573941
Abdullah Hassan <65224472+abdullah12-alt@users.noreply.github.com>
2024-12-07 09:31:54 +0500

    12/7/24

---
+++ Typescript/JavaScript/day9/loops.js
@@ -0,0 +1,9 @@
+for(let i=0; i<10; i=+2){
+    console.log(i)
+}
+
+// i=+5
+// i = i+5
+
+// i++ 
+// i = i+1
---
+++ Typescript/JavaScript/day9/switch.js
@@ -0,0 +1,48 @@
+// const day = 2
+// if (day ==1 ){
+//     console.log("Monday")
+// }
+// else if (day ==2 ){
+//     console.log("Tuesday")
+// }
+// else if (day ==3 ){
+//     console.log("Wednesday")
+// }
+// else if (day ==4 ){
+//     console.log("Thursday")
+// }
+// else if (day == 5 ){
+//     console.log("Friday")
+// }
+// else if (day ==6 ){
+//     console.log("Saturday")
+// }
+// else if (day ==7 ){
+//     console.log("Sunday")
+// }
+
+const day = 7
+
+switch(day){
+    case 1:
+        console.log("Monday")
+        break
+    case 2:
+        console.log("Tuesday")
+        break
+    case 3:
+        console.log("Wed")
+        break
+    case 4:
+        console.log("Thursday")
+        break
+    case 5:
+        console.log("Friday")
+        break
+    case 6:
+        console.log("sat")
+        break
+    default:
+        console.log("sunday")
+        break
+}
